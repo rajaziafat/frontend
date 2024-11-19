87007d6479e8fd909bebd30e37bea7aefefaf5cd
shumail rafique <shumailrafique.info@gmail.com>
2024-11-20 02:37:01 +0500

    changes on /issue page

--- src/components/issue/Action.tsx
+++ src/components/issue/Action.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Issue, ManagedIssueState } from "src/model";
-import { ButtonSize, ButtonType, ExternalLinkButton, LinkButton } from "src/components";
-import { Audience } from "src/views";
+// import { ButtonSize, ButtonType, ExternalLinkButton, LinkButton } from "src/components";
+// import { Audience } from "src/views";
 import { fundIssuePath } from "src/App";
-import { DeveloperButton, SecondaryDeveloperButton } from "../Buttons/Buttons";
+// import { DeveloperButton, SecondaryDeveloperButton } from "../Buttons/Buttons";
+import { Button } from "../elements/Button";
+import { Link } from "react-router-dom";
 
 interface ActionProps {
   issue: Issue;
@@ -14,15 +16,23 @@ interface ActionProps {
 export function Action(props: ActionProps) {
   console.log(props.successfullyFunded);
   const actOnIssueButton = (
-    <LinkButton
-      linkProps={{ to: fundIssuePath(props.issue.id.repositoryId.ownerId.login, props.issue.id.repositoryId.name, props.issue.id.number) }}
-      buttonProps={{
-        htmlButtonProps: { children: "ACT ON ISSUE" },
-        type: props.successfullyFunded ? ButtonType.SECONDARY : ButtonType.PRIMARY,
-        audience: Audience.DEVELOPER,
-        size: ButtonSize.MEDIUM,
-      }}
-    />
+    <>
+      {/* <LinkButton
+        linkProps={{ to: fundIssuePath(props.issue.id.repositoryId.ownerId.login, props.issue.id.repositoryId.name, props.issue.id.number) }}
+        buttonProps={{
+          htmlButtonProps: { children: "ACT ON ISSUE" },
+          type: props.successfullyFunded ? ButtonType.SECONDARY : ButtonType.PRIMARY,
+          audience: Audience.DEVELOPER,
+          size: ButtonSize.MEDIUM,
+        }}
+      /> */}
+      <Button variant={props.successfullyFunded ? "SECONDARY_DEVELOPER" : "PRIMARY_DEVELOPER"} size="MEDIUM" asChild>
+        <Link to={fundIssuePath(props.issue.id.repositoryId.ownerId.login, props.issue.id.repositoryId.name, props.issue.id.number)}>
+          <span className="relative z-20">ACT ON ISSUE</span>
+        </Link>
+      </Button>
+    </>
+
     // <>
     //   {props.successfullyFunded ? (
     //     <DeveloperButton
@@ -50,15 +60,22 @@ export function Action(props: ActionProps) {
         {actOnIssueButton}
 
         {props.successfullyFunded && (
-          <ExternalLinkButton
-            href={props.issue.htmlUrl}
-            buttonProps={{
-              htmlButtonProps: { children: "ACT ON GITHUB" },
-              type: props.successfullyFunded ? ButtonType.PRIMARY : ButtonType.SECONDARY,
-              size: ButtonSize.MEDIUM,
-              audience: Audience.DEVELOPER,
-            }}
-          />
+          <>
+            {/* <ExternalLinkButton
+              href={props.issue.htmlUrl}
+              buttonProps={{
+                htmlButtonProps: { children: "ACT ON GITHUB" },
+                type: props.successfullyFunded ? ButtonType.PRIMARY : ButtonType.SECONDARY,
+                size: ButtonSize.MEDIUM,
+                audience: Audience.DEVELOPER,
+              }}
+            /> */}
+            <Button variant={props.successfullyFunded ? "SECONDARY_DEVELOPER" : "PRIMARY_DEVELOPER"} size="MEDIUM" asChild>
+              <Link to={props.issue.htmlUrl}>
+                <span className="relative z-20">ACT ON GITHUB</span>
+              </Link>
+            </Button>
+          </>
         )}
 
         {/* {props.successfullyFunded ? (
@@ -71,7 +88,7 @@ export function Action(props: ActionProps) {
   } else if (props.state === ManagedIssueState.REJECTED || props.state === ManagedIssueState.SOLVED) {
     return (
       <div>
-        <h3 className=" underline italic px-md-5">Closed</h3>
+        <h3 className="px-md-5 italic underline">Closed</h3>
       </div>
     );
   } else {
